34a0e510aa7c6ba4820a65eeacb96822d63067cc
Sergey <extrem2014@gmail.com>
2024-10-25 12:24:06 +0700

    start work 25.10

--- sript.js
+++ sript.js
@@ -1,23 +1,73 @@
 'use strict'
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', 0)
+// let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', 0)
 
-console.log(numberOfFilms)
+// console.log(numberOfFilms)
 
-let personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
+// let personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false,
+// }
+
+// const a = prompt('Один из последних просмотренных фильмов?'),
+//       b = +prompt('Насколько оцените его?')
+//       c = prompt('Один из последних просмотренных фильмов?'),
+//       d = +prompt('Насколько оцените его?')
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB)
+
+  let i = 2;
+
+  while (i <= 16) {
+    if (i % 2 !== 0) console.log(i)
+    i++
+  }
+
+for (let i = 2; i <= 16; i++) {
+      if (i % 2 === 0) {
+          continue;
+      } else {
+          console.log(i);
+      }
+  }
+
+let d = 5
+
+console.log(typeof(d))
+
+function secondTask() {
+  // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+  const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+  // Пишем решение вот тут
+  for (let i = 0; i < data.length; i++) {
+      if (typeof(data[i]) == "number") data[i] = data[i] * 2
+      if (typeof(data[i]) == "string") data[i] = data[i] + ' - done'
+  }
+  
+  // Не трогаем
+  return data;
+}
+console.log(secondTask())
+
+function thirdTask() {
+  // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+  const data = [5, 10, 'Shopping', 20, 'Homework'];
+  const result = [];
+
+  // Пишем решение вот тут
+  for (let i =  1; i <=  data.length; i++) {
+      result[i-1] = data[data.length - i];
+  }
+  
+  // Не трогаем
+  return result;
 }
 
-const a = prompt('Один из последних просмотренных фильмов?'),
-      b = +prompt('Насколько оцените его?')
-      c = prompt('Один из последних просмотренных фильмов?'),
-      d = +prompt('Насколько оцените его?')
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB)
+console.log(thirdTask())
